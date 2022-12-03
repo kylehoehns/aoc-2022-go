afad59c5dd1f27e5c3bcaa8c2a1915fc0564d5e0
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2022-12-03 19:55:30 +0000

    add solution  for day 3

---
+++ puzzles/day03/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+
+	t.Run("Part 1", func(t *testing.T) {
+		actual := part1("./sample_input.txt", createPriorities())
+		expected := 157
+		if actual != expected {
+			t.Fail()
+			t.Logf("Expected %d but actual was %d", expected, actual)
+		}
+	})
+
+}
+
+func TestPart2(t *testing.T) {
+
+	t.Run("Part 2", func(t *testing.T) {
+		actual := part2("./sample_input.txt", createPriorities())
+		expected := 70
+		if actual != expected {
+			t.Fail()
+			t.Logf("Expected %d but actual was %d", expected, actual)
+		}
+	})
+
+}
