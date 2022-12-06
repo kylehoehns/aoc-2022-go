8ec53a4425ad5f5d09742e6fe1936ee692bbe480
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2022-12-06 07:51:02 -0600

    solutions for day 6

--- puzzles/day06/main.go
+++ puzzles/day06/main.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/kylehoehns/aoc-2022-go/utils/files"
+	"github.com/kylehoehns/aoc-2022-go/utils/strings"
 )
 
 func main() {
-	fmt.Println("Part 1: ", part1("input.txt"))
-	fmt.Println("Part 2: ", part2("input.txt"))
+	input := files.Read("input.txt")
+	fmt.Println("Part 1: ", part1(input))
+	fmt.Println("Part 2: ", part2(input))
 }
 
-func part1(name string) string {
-	return "todo"
+func part1(name string) int {
+	i, done := findMarkerIndex(name, 4)
+	if done {
+		return i
+	}
+
+	panic("Input did not have any valid marker")
 }
 
-func part2(name string) string {
-	return "todo"
+func part2(name string) int {
+	i, done := findMarkerIndex(name, 14)
+	if done {
+		return i
+	}
+
+	panic("Input did not have any valid marker")
+}
+
+func findMarkerIndex(name string, digits int) (int, bool) {
+	for i := digits; i < len(name); i++ {
+		marker := name[i-digits : i]
+		if strings.HasAllUniqueRunes(marker) {
+			return i, true
+		}
+	}
+	return 0, false
 }
