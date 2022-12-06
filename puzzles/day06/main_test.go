bb360f2a26520dae728d0b4963c114feeffa710d
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2022-12-05 20:46:20 -0600

    add day 6 boilerplate

---
+++ puzzles/day06/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+
+	t.Run("Part 1", func(t *testing.T) {
+		actual := part1("./sample_input.txt")
+		expected := "todo"
+		if actual != expected {
+			t.Errorf("Expected %s but actual was %s", expected, actual)
+		}
+	})
+
+}
+
+func TestPart2(t *testing.T) {
+
+	t.Run("Part 2", func(t *testing.T) {
+		actual := part2("./sample_input.txt")
+		expected := "todo"
+		if actual != expected {
+			t.Errorf("Expected %s but actual was %s", expected, actual)
+		}
+	})
+
+}
